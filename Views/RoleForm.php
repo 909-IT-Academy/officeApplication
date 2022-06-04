ece4fda905d28fb43684442875da238769f51fda
abeeralll <abiralshrestha337@gmail.com>
2022-06-05 09:42:04 +1000

    role placeholder added

--- Views/RoleForm.php
+++ Views/RoleForm.php
@@ -6,7 +6,7 @@
       <form action="" method="post">
         <div class="mb-3">
           <label for="role_name" class="form-label">Role</label>
-          <input type="text" class="form-control" name="role_name" value="<?php  ?>">
+          <input type="text" class="form-control" name="role_name" value="<?php  ?>" placeholder="Role">
         </div> <br>
 
         <button type="submit" class="btn btn-primary">Submit</button>
