b02777bf60f7bc0b872c420c1ae674007d9af299
abeeralll <abiralshrestha337@gmail.com>
2022-06-12 10:02:58 +1000

    changes

--- login.php
+++ login.php
@@ -27,48 +27,34 @@ session_start();
         <div class="row" style="min-height:100vh">
             <div class="d-flex justify-content-center align-items-center">
                 <div class="p-2 bd-highlight">
-                    
-
-
-                    <form name="loginForm" action="chooseRole.php" method="POST" class="p-5 border">
+             <form name="loginForm" action="chooseRole.php" method="POST" class="p-5 border">
                         <div class="mb-3 ">
                             <div class="error_pacement">
                                 <div class="d-flex flex-row">
                                     <div class="error_sign" id="er_icon">
                                     <i class="fa-solid fa-triangle-exclamation"></i> 
-                                   
-                                    </div>
+                                   </div>
                                     <div class="d-flex px-2 flex-column bd-highlight mb-3 ">
                                         <span class="error_sign" id="error"></span>
                                         <span class="received_error" id="error1"></span>
                                         <span class="received_error"id="error2"></span>
-
                                     </div>
                                 </div>
-
                             </div>
                         </div>
-
-
                         <div class="mb-3">
                             <label for="email" class="form-label" name="email">Email address</label>
                             <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp">
-
-                        </div>
-
-                        <div class="mb-3">
+                         </div>
+                          <div class="mb-3">
                             <label for="password" class="form-label">Password</label>
                             <input type="password" class="form-control" id="password" name="password">
                         </div>
                         <div class="d-grid gap-2">
                             <input class="btn btn-primary" type="submit" name="submit" value ="SUBMIT">
-
-                        </div>
-
-
-                    </form>
-
-                </div>
+                         </div>
+                        </form>
+                    </div>
             </div>
         </div>
     </div>
